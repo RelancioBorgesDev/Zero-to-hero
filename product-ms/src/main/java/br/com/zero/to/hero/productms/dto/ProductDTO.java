d3a8a858fe1c3a643da98922a5adbb0d285154ab
RelancioBorgesDev <relancio.dev@gmail.com>
2023-01-26 16:27:37 -0300

    feat: Adicionado validacao do ProductDTO

--- product-ms/src/main/java/br/com/zero/to/hero/productms/dto/ProductDTO.java
+++ product-ms/src/main/java/br/com/zero/to/hero/productms/dto/ProductDTO.java
@@ -1,14 +1,21 @@
 package br.com.zero.to.hero.productms.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class ProductDTO {
 
+    @NotBlank(message = "O campo nome é obrigatório !")
     private String name;
 
+    @Size(min = 50)
     private String description;
 
+    @Positive
     private double price;
 
     private boolean available;
